72afd236205340f7a13c728d6a6218f6909bca9f
walae04 <walae.ouahi@gmail.com>
2025-11-10 08:44:49 +0100

    First commit

---
+++ src/main/kotlin/org/ldv/ecommerce_fragrances/ECommerceFragrancesApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.ecommerce_fragrances
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceFragrancesApplication
+
+fun main(args: Array<String>) {
+    runApplication<ECommerceFragrancesApplication>(*args)
+}
